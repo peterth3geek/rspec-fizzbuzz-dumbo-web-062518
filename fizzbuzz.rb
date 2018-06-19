ba5f09d791078f8e6013a21293ad4a6aa9a60a27
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-19 18:27:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz (int)
-if int % 3 == 0
+  if int % 5 == 0 || int % 3 == 0
+    p "FizzBuzz"
+elsif int % 3 == 0
   p "Fizz"
 elsif int % 5 == 0
   p "Buzz"
-elsif int % 5 == 0 || int % 3 == 0
+if int % 5 == 0 || int % 3 == 0
   p "FizzBuzz"
-
 end
 end
